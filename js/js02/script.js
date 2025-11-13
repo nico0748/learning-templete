ed5b751ac2ca7e173242a13d74d97368c75374cf
Yuma Hosozawa <yuma.sakahiga@gmail.com>
2025-11-14 00:43:53 +0900

    add: JSの定数・変数及び演算に関するテンプレートを追加

---
+++ js/js02/script.js
@@ -0,0 +1,16 @@
+console.log('Hello World!!');
+console.log((10 + 5) * 2 / 5 + 4);  // → 10
+console.log(-5 + 8);  // → 3
+console.log(2 ** 3);  // → 8
+console.log(10 % 3);  // → 1（10÷3は3余り1）
+
+const weight = 50;
+const height = 1.55;
+console.log('体重' + weight + 'kg／身長' + height * 100 + 'cmのBMI：');
+console.log(weight / (height * height)); // → 20.20..
+
+let x = 0;
+x++;
+console.log(x); // → 1
+x--;
+console.log(x); // → 0
